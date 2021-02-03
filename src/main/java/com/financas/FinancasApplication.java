2821711448e06652f1969e25f4e6e631d198a277
LuanaDG <luanadelagiustina@gmail.com>
2021-02-03 17:03:59 -0300

    ajustes

--- src/main/java/com/financas/FinancasApplication.java
+++ src/main/java/com/financas/FinancasApplication.java
@@ -2,10 +2,21 @@ package com.financas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-public class FinancasApplication {
+@EnableSwagger2
+public class FinancasApplication implements WebMvcConfigurer{
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FinancasApplication.class, args);
 	}
