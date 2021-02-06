2888a34e2ee248a9f41f36fb95721a4a44a4b98e
LuanaDG <luanadelagiustina@gmail.com>
2021-02-06 15:27:45 -0300

    ajuste

--- src/main/java/com/financas/repository/LancamentoRepository.java
+++ src/main/java/com/financas/repository/LancamentoRepository.java
@@ -13,8 +13,10 @@ import com.financas.entity.enums.TipoLancamento;
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>{
 
 	@Query(value = " select sum(l.valor) from Lancamento l join l.usuario u "
-			+ "where u.id = :idUsuario and l.tipo =:tipo group by u")
-	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus( @Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo );
+			+ "where u.id =:idUsuario and l.tipo =:tipo and l.status =:status group by u")
+	BigDecimal obterSaldoPorTipoLancamentoEUsuarioEStatus( @Param("idUsuario") Long idUsuario, 
+														   @Param("tipo") TipoLancamento tipo,
+														   @Param("status") StatusLancamento status);
 
 	
 }
